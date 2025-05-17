8f3019adfbbeacda920d3fed9e963dd34dcae1c6
at2er <xb0515@outlook.com>
2025-05-17 11:15:39 +0800

    mod(colors): simple-night

--- colors/simple-night.h
+++ colors/simple-night.h
@@ -7,9 +7,9 @@ static const char col_norm_bg[]     = "#000000";
 static const char col_norm_border[] = "#000000";
 static const char col_norm_fg[]     = "#bbbbbb";
 
-static const char col_sel_bg[]     = "#4444aa";
+static const char col_sel_bg[]     = "#000000";
 static const char col_sel_border[] = "#4444aa";
-static const char col_sel_fg[]     = "#bbbbbb";
+static const char col_sel_fg[]     = "#4444aa";
 
 static const char col_title_bg[]     = "#000000";
 static const char col_title_border[] = "#000000";
